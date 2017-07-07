8806de4f8e2d5fa08fc6fa076aaa6973b1b0e57f
harsh jain <harsh.jain@jginfosystems.com>
2017-07-07 12:04:19 +0530

    added GSTIN to partner and company

--- res_partner.py
+++ res_partner.py
@@ -1,11 +1,12 @@
 from openerp.osv import fields, osv
 
-class res_partner(osv.osv):
+class res_company(osv.osv):
     _inherit = "res.company"
     _description = "Adding Fields"
     _columns = {
                 'mobile':fields.char('Mobile'),
-                'tin':fields.char('TIN',size=15)
+                'tin':fields.char('TIN',size=15),
+                'gstin':fields.char("GSTIN",size=16)
                 }
 
 
@@ -13,8 +14,10 @@ class res_partner(osv.osv):
 class res_partner(osv.osv):
     _inherit = "res.partner"
     _description = "Adding Fields"
-    _columns = {'street': fields.char('Street',size=60),
+    _columns = {
+                'street': fields.char('Street',size=60),
                 'street2': fields.char('Street2',size=60),
                 'tin':fields.char('TIN',size=14),
-                'cst':fields.char('CST',size=14)
+                'cst':fields.char('CST',size=14),
+                'gstin':fields.char("GSTIN",size=16)
                 }
